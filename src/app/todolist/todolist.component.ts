c84816c0ab9ff7edd376202f3b0c2493150f9525
Szymon Wrzesień <szymon.wrzesien09@gmail.com>
2019-11-13 13:55:13 +0100

    add component todolist

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,49 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.scss']
+})
+export class TodolistComponent {
+  newTodoTitle = '';
+
+  todos: Array<Todo> = [
+                        {
+                          title: 'zakupy',
+                          complete: true
+                        },
+                        {
+                          title: 'kino',
+                          complete: false
+                        }
+                      ];
+
+addTodo() {
+  if (!this.newTodoTitle) {
+    return;
+  }
+  const newTodo: Todo = {
+    title: this.newTodoTitle,
+    complete: false
+  };
+  this.todos.push(newTodo);
+  this.newTodoTitle = '';
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index,1);
+    }
+
+
+
+}
+
+
+
+
+interface Todo {
+  title: string;
+  complete: boolean;
+
+}
